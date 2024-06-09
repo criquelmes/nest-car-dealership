a74af916043ef3e8efbda6934c7ea50d2b042e2d
Christopher Riquelme <criquelmes@live.cl>
2024-06-09 19:41:58 -0400

    Fin sección 3: Primeros pasos en nest

---
+++ src/cars/cars.controller.ts
@@ -0,0 +1,51 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { CarsService } from './cars.service';
+
+@Controller('cars')
+export class CarsController {
+  constructor(private readonly carsService: CarsService) {}
+
+  @Post()
+  createCar(@Body() body: any) {
+    return {
+      body,
+    };
+  }
+
+  @Get()
+  getAllCars() {
+    return this.carsService.findAll();
+  }
+
+  @Get(':id')
+  getCarById(@Param('id', ParseIntPipe) id: number) {
+    console.log({ id });
+    const car = this.carsService.findOne(id);
+    return car;
+  }
+
+  @Patch(':id')
+  updateCar(@Body() body: any, @Param('id', ParseIntPipe) id: number) {
+    return {
+      body,
+      id,
+    };
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      id,
+      method: 'delete',
+    };
+  }
+}
